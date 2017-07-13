0ae5ec595e95d4d8b73658c103dfe614bc19ea12
Phil Lang <plangphoto@gmail.com>
2017-07-13 13:25:14 +0100

    added functionality and tests for initializing a journey object with both entry and exit station instance variables

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,4 +1,9 @@
 class Journey
 
+  attr_reader :exit_station, :entry_station
 
+  def initialize
+    @entry_station = nil
+    @exit_station = nil
+  end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,4 +2,16 @@ require 'journey'
 
 describe Journey do
   subject(:journey) { described_class.new }
+  let(:station_one) { double(:station_one, :name => 'Victoria', :zone => 1) }
+  let(:station_two) { double(:station_two, :name => 'Hammersmith', :zone => 1) }
+
+  describe 'the creation of a new journey' do
+    it 'a new journey to have an empty entry station' do
+      expect(journey.entry_station).to eq nil
+    end
+
+    it 'a new journey to have an empty exit station' do
+      expect(journey.exit_station).to eq nil
+    end
+  end
 end
