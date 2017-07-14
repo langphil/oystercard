81b4b1c106c344672b83dc3a06414920fb1e5fc8
Phil Lang <plangphoto@gmail.com>
2017-07-14 12:07:48 +0100

    made updates to readme

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 ## Oystercard
 
+This project is about testing functionality around a transport card system.
+
 ## What is this
 
 ## Issues
--- spec/feature_spec.rb
+++ spec/feature_spec.rb
@@ -1,31 +1,19 @@
 require './lib/oystercard'
 
 card = Oystercard.new 10
-journey = Journey.new
 station1 = Station.new 'Bank', 2
 station2 = Station.new 'Holland Park', 2
 station3 = Station.new 'Victoria', 1
 station4 = Station.new 'Clapham Junction', 3
+
 p card
+
 card.touch_in station1
 card.touch_out station2
+
 card.touch_in station3
 card.touch_out station4
-p card
 
-
-=begin
-station_one = Station.new 'Kenginston Olympia', 2
-station_two = Station.new 'Shepherds Bush', 2
-station_three = Station.new 'Victoria', 1
-station_four = Station.new 'Clapham Junction', 2
-p card
-
-card.touch_in station_one
-card.touch_out station_two
-
-card.touch_in station_three
-card.touch_out station_four
+card.top_up 10
 
 p card
-=end
